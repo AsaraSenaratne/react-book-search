cbec2af9dffca56f8458f7f7d60ea0fc10c7abe1
Dilan Srilal <dilan@taskanalytics.com>
2017-06-10 09:34:05 +0530

    Basic books container flex styles added

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react'
-
+import placeHolder from './logo.svg'
 
 class Book extends Component{
 	render(){
 		const {book} = this.props
 		return(
-			<div>
+			<div className="book">
 				<h3> {book.title} </h3>
 				<p> {book.authors ? book.authors.join(',') : '<not provided>'} </p>
-				<img src={book.imageLinks.thumbnail} />
-				<p> {book.description} </p>
+				<img src={book.imageLinks ? book.imageLinks.thumbnail : placeHolder} />
+				<p> {book.description ? book.description.substring(0,250) : ''} </p>
 			</div>
 		)
 	}
--- src/BooksContainer.js
+++ src/BooksContainer.js
@@ -2,13 +2,10 @@ import React, {Component} from 'react'
 import Book from './Book'
 
 class BooksContainer extends Component{
-	constructor(){
-		super()
-	}
 	render(){
 		const {books} = this.props
 		return(
-			<div>
+			<div className="books-container">
 				{books.map(b => <Book book={b.volumeInfo} />)}
 			</div>
 		)
